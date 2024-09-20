0d25f51354f380f2c384745d7cdfb91e70029662
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-09-20 18:14:29 +0800

    Refactor code to update user stats links

--- resources/views/livewire/user-stats.blade.php
+++ resources/views/livewire/user-stats.blade.php
@@ -1,41 +1,41 @@
 <div class="row">
     <div class="col-xxl-4 col-md-4">
         <div class="card info-card sales-card">
-            <div class="card-body">
-                <h5 class="card-title">Users</h5>
+            <a href="{{ route('users') }}">
+                <div class="card-body">
+                    <h5 class="card-title">Users</h5>
 
-                <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i class="bi bi-people"></i>
-                    </div>
-                    <div class="ps-3 mt-2 pagetitle">
-                        <h1>{{ $usersCount }}</h1>
-                        <span class="text-muted small pt-2 ps-1">total</span>
+                    <div class="d-flex align-items-center">
+                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                            <i class="bi bi-people"></i>
+                        </div>
+                        <div class="ps-3 mt-2 pagetitle">
+                            <h1>{{ $usersCount }}</h1>
+                            <span class="text-muted small pt-2 ps-1">total</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+            </a>
         </div>
     </div>
 
 
     <div class="col-xxl-4 col-md-4">
         <div class="card info-card present-card">
-
-            <div class="card-body">
-                <h5 class="card-title">Instructors</h5>
-
-                <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i class="bi bi-person"></i>
-                    </div>
-                    <div class="ps-3 mt-2 pagetitle">
-                        <h1>{{ $instructorsCount }}</h1>
-                        <span class="text-muted small pt-2 ps-1">total</span>
+            <a href="{{ route('faculties') }}">
+                <div class="card-body">
+                    <h5 class="card-title">Instructors</h5>
+                    <div class="d-flex align-items-center">
+                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                            <i class="bi bi-person"></i>
+                        </div>
+                        <div class="ps-3 mt-2 pagetitle">
+                            <h1>{{ $instructorsCount }}</h1>
+                            <span class="text-muted small pt-2 ps-1">total</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+            </a>
         </div>
     </div>
 
@@ -43,7 +43,7 @@
     <div class="col-xxl-4 col-md-4">
 
         <div class="card info-card customers-card">
-
+            <a href="{{ route('students') }}">
             <div class="card-body">
                 <h5 class="card-title">Students</h5>
 
@@ -58,6 +58,7 @@
                 </div>
 
             </div>
+            </a>
         </div>
 
     </div>
