be18e2d0ee429781cd521548c594311bb146753a
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-09-24 00:06:29 +0800

    Refactor subject table to add table-hover class

--- resources/views/livewire/subject-table.blade.php
+++ resources/views/livewire/subject-table.blade.php
@@ -74,7 +74,7 @@
     </div>
 
     <div class="overflow-auto">
-        <table class="table">
+        <table class="table table-hover">
             <thead>
                 <tr>
                     <th scope="col">#</th>
