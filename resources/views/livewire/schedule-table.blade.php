2ba5cb6476e5f0910f822201365d1bc593baef99
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-10-11 12:58:12 +0800

    Refactor ScheduleTable component to display schedules based on user role

--- resources/views/livewire/schedule-table.blade.php
+++ resources/views/livewire/schedule-table.blade.php
@@ -50,9 +50,9 @@
             </div>
         </div>
         @if (Auth::user()->isAdmin())
-        <div class="col-12 col-md-2">
-            <livewire:create-schedule />
-        </div>
+            <div class="col-12 col-md-2">
+                <livewire:create-schedule />
+            </div>
         @endif
     </div>
 
@@ -106,14 +106,14 @@
                         'displayName' => 'End Time',
                     ])
                     @if (Auth::user()->isAdmin())
-                    <th scope="col" class="text-center text-dark fw-semibold">Action</th>
+                        <th scope="col" class="text-center text-dark fw-semibold">Action</th>
                     @endif
                 </tr>
             </thead>
             <tbody>
                 @foreach ($schedules as $key => $schedule)
                     <tr wire:key="{{ $schedule->id }}"
-                        onclick="window.location='{{ route('schedule.view', $schedule->id) }}';"
+                        onclick="window.location='{{ Auth::user()->isAdmin() ? route('schedule.view', $schedule->id) : route('class.view', $schedule->id) }}';"
                         style="cursor: pointer;">
                         <th scope="row">{{ $key + 1 }}</th>
                         <td>{{ $schedule->schedule_code }}</td>
@@ -128,24 +128,26 @@
                         <td>{{ Carbon::parse($schedule->start_time)->format('h:i A') }}</td>
                         <td>{{ Carbon::parse($schedule->end_time)->format('h:i A') }}</td>
                         @if (Auth::user()->isAdmin())
-                        <td class="text-center">
-                            <div class="btn-group dropstart">
-                                <a class="icon" href="#" data-bs-toggle="dropdown" aria-expanded="false"
-                                    onclick="event.stopPropagation()">
-                                    <i class="bi bi-three-dots"></i>
-                                </a>
-                                <ul class="dropdown-menu table-action table-dropdown-menu-arrow me-3"
-                                    onclick="event.stopPropagation()">
-                                    <li><button type="button" class="dropdown-item" href="#">View</button></li>
-                                    <li><button @click="$dispatch('edit-mode',{id:{{ $schedule->id }}})" type="button"
-                                            class="dropdown-item" data-bs-toggle="modal"
-                                            data-bs-target="#verticalycentered">Edit</button></li>
-                                    <li><button wire:click="delete({{ $schedule->id }})"
-                                            wire:confirm="Are you sure you want to delete this schedule?" type="button"
-                                            class="dropdown-item text-danger" href="#">Delete</button>
-                                </ul>
-                            </div>
-                        </td>
+                            <td class="text-center">
+                                <div class="btn-group dropstart">
+                                    <a class="icon" href="#" data-bs-toggle="dropdown" aria-expanded="false"
+                                        onclick="event.stopPropagation()">
+                                        <i class="bi bi-three-dots"></i>
+                                    </a>
+                                    <ul class="dropdown-menu table-action table-dropdown-menu-arrow me-3"
+                                        onclick="event.stopPropagation()">
+                                        <li><button type="button" class="dropdown-item" href="#">View</button>
+                                        </li>
+                                        <li><button @click="$dispatch('edit-mode',{id:{{ $schedule->id }}})"
+                                                type="button" class="dropdown-item" data-bs-toggle="modal"
+                                                data-bs-target="#verticalycentered">Edit</button></li>
+                                        <li><button wire:click="delete({{ $schedule->id }})"
+                                                wire:confirm="Are you sure you want to delete this schedule?"
+                                                type="button" class="dropdown-item text-danger"
+                                                href="#">Delete</button>
+                                    </ul>
+                                </div>
+                            </td>
                         @endif
                     </tr>
                 @endforeach
